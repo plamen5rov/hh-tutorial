b4cfb98d8aea205de76cdc987230a916e96ff622
Plamen <biznetmen@gmail.com>
2022-12-03 13:33:47 +0200

    test refactoring - duplication code removed by using fixture

--- test/Token.js
+++ test/Token.js
@@ -1,31 +1,42 @@
 const { ethers } = require("hardhat");
 
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect } = require("chai");
 
 describe("Token contract", function () {
-  it("Deployment should assign the total supply of tokens to the owner", async function () {
-    const [owner] = await ethers.getSigners();
-
+  async function deployTokenFixture() {
     const Token = await ethers.getContractFactory("Token");
+    const [owner, addr1, addr2] = await ethers.getSigners();
 
     const hardhatToken = await Token.deploy();
+
+    await hardhatToken.deployed();
+
+    // Fixtures can return anything you consider useful for your tests
+    return { Token, hardhatToken, owner, addr1, addr2 };
+  }
+
+  it("Should assign the total supply of tokens to the owner", async function () {
+    const { hardhatToken, owner } = await loadFixture(deployTokenFixture);
 
     const ownerBalance = await hardhatToken.balanceOf(owner.address);
     expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
   });
+
   it("Should transfer tokens between accounts", async function () {
-    const [owner, addr1, addr2] = await ethers.getSigners();
-
-    const Token = await ethers.getContractFactory("Token");
-
-    const hardhatToken = await Token.deploy();
+    const { hardhatToken, owner, addr1, addr2 } = await loadFixture(
+      deployTokenFixture
+    );
 
     // Transfer 50 tokens from owner to addr1
-    await hardhatToken.transfer(addr1.address, 50);
-    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
+    await expect(
+      hardhatToken.transfer(addr1.address, 50)
+    ).to.changeTokenBalances(hardhatToken, [owner, addr1], [-50, 50]);
 
     // Transfer 50 tokens from addr1 to addr2
-    await hardhatToken.connect(addr1).transfer(addr2.address, 50);
-    expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50);
+    // We use .connect(signer) to send a transaction from another account
+    await expect(
+      hardhatToken.connect(addr1).transfer(addr2.address, 50)
+    ).to.changeTokenBalances(hardhatToken, [addr1, addr2], [-50, 50]);
   });
 });
