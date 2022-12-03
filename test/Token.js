beffc7946a5a85fa7e1414ea62236f433b60c44a
Plamen <biznetmen@gmail.com>
2022-12-03 13:30:48 +0200

    second test added

--- test/Token.js
+++ test/Token.js
@@ -1,3 +1,5 @@
+const { ethers } = require("hardhat");
+
 const { expect } = require("chai");
 
 describe("Token contract", function () {
@@ -10,5 +12,20 @@ describe("Token contract", function () {
 
     const ownerBalance = await hardhatToken.balanceOf(owner.address);
     expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+  });
+  it("Should transfer tokens between accounts", async function () {
+    const [owner, addr1, addr2] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("Token");
+
+    const hardhatToken = await Token.deploy();
+
+    // Transfer 50 tokens from owner to addr1
+    await hardhatToken.transfer(addr1.address, 50);
+    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
+
+    // Transfer 50 tokens from addr1 to addr2
+    await hardhatToken.connect(addr1).transfer(addr2.address, 50);
+    expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50);
   });
 });
